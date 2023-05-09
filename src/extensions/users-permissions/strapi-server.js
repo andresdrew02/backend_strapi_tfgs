3870990833e1271e85b30d5a368a0a409e8e00df
andresdrew02 <alumno.254958@ies-azarquiel.es>
2023-05-09 22:08:58 +0200

    updated users-permissions extensions
    added updateMe option to update this user

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -3,7 +3,7 @@
 module.exports = (plugin) => {
     //update recien creado
     plugin.controllers.user.updateRecien = async (ctx) => {
-        if (!ctx.state.user || !ctx.state.user.id){
+        if (!ctx.state.user || !ctx.state.user.id) {
             return ctx.response.status = 401
         }
         await strapi.query('plugin::users-permissions.user').update({
@@ -27,5 +27,49 @@ module.exports = (plugin) => {
             }
         }
     )
+
+    plugin.controllers.user.updateMe = async (ctx) => {
+        if (!ctx.state.user || !ctx.state.user.id) {
+            return ctx.response.status = 401
+        }
+
+        if (ctx.request.body.pais === undefined || ctx.request.body.ciudad === undefined ||
+            ctx.request.body.poblacion === undefined || ctx.request.body.via === undefined ||
+            ctx.request.body.calle === undefined || ctx.request.body.num === undefined ||
+            ctx.request.body.cp === undefined || ctx.request.body.portal === undefined) {
+            return ctx.response.status = 400
+        }
+
+        await strapi.entityService.update('plugin::users-permissions.user', ctx.state.user.id, {
+            data: {
+                nombre_completo: ctx.request.body.nombre_completo,
+                direccion: {
+                    calle: ctx.request.body.calle,
+                    tipo_via: ctx.request.body.via,
+                    numero: ctx.request.body.num,
+                    cp: ctx.request.body.cp,
+                    ciudad: ctx.request.body.ciudad,
+                    poblacion: ctx.request.body.poblacion,
+                    pais: ctx.request.body.pais,
+                    portal: ctx.request.body.portal
+                }
+            }
+        }).then((res) => {
+            ctx.response.status = 200
+        })
+    }
+
+    plugin.routes['content-api'].routes.push(
+        {
+            method: 'PUT',
+            path: '/user/me/update',
+            handler: 'user.updateMe',
+            config: {
+                prefix: '',
+                policies: []
+            }
+        }
+    )
+
     return plugin
 }
